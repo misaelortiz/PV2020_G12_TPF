2922b74f404392335693c25cee6b337109b7bdcf
ORTIZ <ORTIZ@DESKTOP-FU9HIQR>
2020-06-17 17:01:59 -0300

    Asunto: Creacion del proyecto Responsable: Ortiz E. Misael * Inicializacion del proyecto final

---
+++ src/main/java/ar/edu/unju/fi/Grupo12TpfApplication.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo12TpfApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo12TpfApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
