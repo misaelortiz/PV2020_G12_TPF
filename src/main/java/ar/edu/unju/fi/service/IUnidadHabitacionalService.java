8bfb1bcbb0572d8069d1bd88584557c0669984e0
arielguti <ar_ma_la@hotmail.com>
2020-06-29 22:47:56 -0300

    vamooss

--- src/main/java/ar/edu/unju/fi/service/IUnidadHabitacionalService.java
+++ src/main/java/ar/edu/unju/fi/service/IUnidadHabitacionalService.java
@@ -3,10 +3,22 @@
  */
 package ar.edu.unju.fi.service;
 
+import ar.edu.unju.fi.model.UnidadHabitacional;
+
 /**
  * @author ArielG
  *
  */
 public interface IUnidadHabitacionalService {
+	
+	public void crear(UnidadHabitacional unaUnidad);
+	
+	public UnidadHabitacional modificar(UnidadHabitacional unUsuario) throws Exception;
+	
+	public void eliminar(String domicilio);
+	
+	public Iterable<UnidadHabitacional> listarTodos();
+	
+	public UnidadHabitacional encontrarUsuario(String domicilio) throws Exception;
 
 }
