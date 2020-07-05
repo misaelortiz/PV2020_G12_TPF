a8fd899a32a8ce4b90e2bc4154637bc48994afd7
arielguti <ar_ma_la@hotmail.com>
2020-07-04 23:35:32 -0300

    dependencias

---
+++ src/main/java/ar/edu/unju/fi/service/BarrioServiceImp.java
@@ -0,0 +1,32 @@
+package ar.edu.unju.fi.service;
+
+import ar.edu.unju.fi.model.Barrio;
+
+public class BarrioServiceImp implements IBarrioService {
+
+	public void crear(Barrio unBarrio) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Barrio modificar(Barrio unBarrio) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void eliminar(String nombre) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Iterable<Barrio> listarBarrios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Barrio encontrarBarrio(String nombre) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
